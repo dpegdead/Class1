c133abc0c981d141c9f8f3082a7177d8e29592b8
dpegdead <45723540+dpegdead@users.noreply.github.com>
2019-12-23 13:18:53 -0600

    Add files via upload

--- Class1.cs
+++ Class1.cs
@@ -1,12 +1,11 @@
-﻿using Oxide.Rust;
-using Facepunch;
+﻿using Facepunch;
 using Oxide;
 
 namespace Oxide.Plugins
 {
     [Info("DPEG PLUGIN", "DPEG", "0.1")]
     [Description("A Plugin Made By DPEG")]
-    class Class1 : RustPlugin
+    class Class1 : CovalencePlugin
     {
         private void Init()
         {
