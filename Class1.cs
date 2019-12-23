0d6f9e1a428a7024d267898967bc795b0507e4e2
dpegdead <45723540+dpegdead@users.noreply.github.com>
2019-12-23 12:58:46 -0600

    Add files via upload

--- Class1.cs
+++ Class1.cs
@@ -5,7 +5,7 @@ using Oxide.Plugins;
 namespace HelloWorldRust
 {
     [Info("Hello World Rust", "DPEG", "0.1")]
-    class HelloWorld : RustPlugin
+    class TestingOfDPEG : RustPlugin
     {
         void Init()
         {
