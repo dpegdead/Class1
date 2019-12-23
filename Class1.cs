1951b5384d0e847b0683c0ed93a96998ef5d9eb5
dpegdead <45723540+dpegdead@users.noreply.github.com>
2019-12-23 12:24:54 -0600

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using Facepunch;
+namespace HelloWorldRust Oxide.Plugins
+{
+    [Info("Hello World Rust", "DPEG", "0.1:)]
+    public class HelloWorld
+    {
+        void Init()
+    {
+            Puts("Init");
+        }
+
+
+    }
+}
